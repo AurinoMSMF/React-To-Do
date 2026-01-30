97a5faaed43fecc65d6cbe73f712cb4d6734c4d4
Aurino <aurinodoc@gmail.com>
2026-01-30 03:12:13 -0300

    Feature: Aplicando boas práticas de adição do atributo 'data-test' nos elementos da aplicação.

--- src/components/TaskTextArea.jsx
+++ src/components/TaskTextArea.jsx
@@ -1,6 +1,7 @@
 function TaskTextArea(props) {
   return (
     <textarea
+      data-test="task-description-textarea"
       className="rounded-md border border-green-200 outline-red-600 px-4 py-2"
       {...props} // Desestrutura todas as props para dentro do elemento (spread)
     ></textarea>
